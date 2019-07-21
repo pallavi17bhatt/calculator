44714636f5a87849c30d925f63980f0e533feafb
pallavi bhatt <pallavibhatt@pallavis-MacBook-Pro.local>
2019-07-21 15:24:42 +0530

    Added feature for multiply, divide, subtraction in calculator app

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -12,21 +12,45 @@ public class Calculator {
       String inputValue = sc.nextLine();
       String command[] = inputValue.split(" ");
       int commandArg = Integer.parseInt(command[1]);
-
-      if ("add".equals(command[0])) {
-        int response = calcy.add(commandArg);
-        System.out.println(response);
-      }
-
+      executeCommand(calcy, command[0], commandArg);
     }
   }
 
-  public int add(int value) {
+  private static void executeCommand(Calculator calcy, String commandName, int commandArg) {
+    if ("add".equals(commandName)) {
+      int response = calcy.add(commandArg);
+      System.out.println(response);
+    } else if ("subtract".equals(commandName)) {
+      int response = calcy.subtract(commandArg);
+      System.out.println(response);
+    } else if ("multiply".equals(commandName)) {
+      int response = calcy.multiply(commandArg);
+      System.out.println(response);
+    } else if ("divide".equals((commandName))) {
+      int response = calcy.divide(commandArg);
+      System.out.println(response);
+    }
+  }
+
+  private int multiply(int value) {
+    answer = answer * value;
+    return answer;
+  }
+
+  private int divide(int value) {
+    if (value == 0)
+      System.out.println("can't divide by zero");
+    else
+      answer = answer / value;
+    return answer;
+  }
+
+  private int add(int value) {
     answer = answer + value;
     return answer;
   }
 
-  public int subtract(int value) {
+  private int subtract(int value) {
     answer = answer - value;
     return answer;
   }
